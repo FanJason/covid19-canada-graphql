b8bd6cc23b736fe6cbe2a3d064951402309e9db7
Jason <jf7776@gmail.com>
2020-04-01 20:57:17 -0700

    add if exists check

--- db/database.ts
+++ db/database.ts
@@ -4,21 +4,21 @@ const pg = require("pg");
 const client = new pg.Client(process.env.CONNECTIONSTR);
 const tableName = "covid19";
 
-const create = "CREATE TABLE IF NOT EXISTS " + tableName + "(id INTEGER NOT NULL PRIMARY KEY, dateLastUpdated VARCHAR(10), discoveryDate VARCHAR(10), gender VARCHAR(15), ageGroup VARCHAR(16), transmission VARCHAR(150), hospitalization VARCHAR(15), ICU VARCHAR(15), status VARCHAR(15))";
-const drop = "DROP TABLE " + tableName;
+const createTable = "CREATE TABLE IF NOT EXISTS " + tableName + "(id INTEGER NOT NULL PRIMARY KEY, dateLastUpdated VARCHAR(10), discoveryDate VARCHAR(10), gender VARCHAR(15), ageGroup VARCHAR(16), transmission VARCHAR(150), hospitalization VARCHAR(15), ICU VARCHAR(15), status VARCHAR(15))";
+const dropTable = "DROP TABLE IF EXISTS " + tableName;
 
 async function connect() {
     try {
         await client.connect();
-        await client.query(drop);
-        await client.query(create);
+        await client.query(dropTable);
+        await client.query(createTable);
         insert(testData);
     } catch (err) {
         console.log(err);
     }
 }
 
-function getInsertQuery(entry) {
+function _getInsertQuery(entry) {
     const keys = Object.keys(entry);
     let start = "INSERT INTO covid19(" + keys[0];
     let end = ") values($" + 1;
@@ -32,7 +32,7 @@ function getInsertQuery(entry) {
 async function insert(data) {
     try {
         data.forEach(async (element) => {
-            const addRow = getInsertQuery(element);
+            const addRow = _getInsertQuery(element);
             const result = await client.query(addRow, Object.values(element));
             console.log(result);
         });
